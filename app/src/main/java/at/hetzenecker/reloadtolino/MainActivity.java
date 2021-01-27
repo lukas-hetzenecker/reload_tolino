92ea15d4d3e4886613e27f8a701e53fba13d3092
Lukas Hetzenecker <lukas@hetzenecker.me>
2021-01-27 21:41:26 +0100

    first commit

---
+++ app/src/main/java/at/hetzenecker/reloadtolino/MainActivity.java
@@ -0,0 +1,22 @@
+package at.hetzenecker.reloadtolino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent("de.telekom.epub.action.STORAGE_CHANGE_EXTERN");
+        intent.putExtra("external", "mounted");
+        sendBroadcast(intent);
+
+        this.finish();
+
+    }
+}
